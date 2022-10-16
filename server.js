c1deb8827276df47f6a06725c3d172a9962826c8
Jordan Hackworth <jhackworth00@outlook.com>
2022-10-15 22:18:12 -0500

    wow

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const session = require("express-session");
+// const session = require("express-session");
 const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
 
@@ -10,19 +10,19 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection.js");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const sess = {
-  secret: process.env.SESSION_SECRET,
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
+// const sess = {
+//   secret: process.env.SESSION_SECRET,
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize,
+//   }),
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
@@ -31,7 +31,8 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public"))); //dont think my azure app is recognizing this static file. So what you need to do instead is  app.use(express.static(path.join(__dirname, "public")));  and then in your html file you need to add the path to the file. So instead of  <link rel="stylesheet" href="style.css">  you need to do  <link rel="stylesheet" href="/style.css">  and then it should work.
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(require("./controllers/"));
 
