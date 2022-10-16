1cfb9bb06401686c206be76490a2f138cc9a2606
Jordan Hackworth <jhackworth00@outlook.com>
2022-10-15 22:55:44 -0500

    soooo close

--- models/Product.js
+++ models/Product.js
@@ -1,42 +1,3 @@
-// "use strict";
-
-// module.exports = (sequelize, DataTypes) => {
-//   const Product = sequelize.define("Product", {
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     category_id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     description: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       validate: {
-//         len: [1],
-//       },
-//     },
-//     quantity: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     price: {
-//       type: DataTypes.DECIMAL,
-//       allowNull: false,
-//     },
-//   });
-//   Product.associate = (models) => {
-//     // associations can be defined here
-//     Product.hasMany(models.Category, {
-//       foreignKey: {
-//         allowNull: false,
-//       },
-//     });
-//   };
-//   return Product;
-// };
-
 const { Sequelize, Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
@@ -72,10 +33,6 @@ Products.init(
   },
   {
     sequelize,
-    // timestamps: false, //this will not create the created_at and updated_at columns in the database table
-    // freezeTableName: true,
-    // underscored: true,
-    // modelName: "products",
   }
 );
 
