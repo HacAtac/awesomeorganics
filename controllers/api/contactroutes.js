a70589e94fa48f55d1e2d1626dd0a5537e66b048
HacAtac <jhackworth00@outlook.com>
2023-04-18 21:00:16 -0500

    Get all contact entries

--- controllers/api/contactroutes.js
+++ controllers/api/contactroutes.js
@@ -16,6 +16,19 @@ router.post("/", (req, res) => {
 }); // Add closing parenthesis and curly brace here
 
 //TODO: Create GET contacts endpoint
+router.get("/", (req, res) => {
+const dbUserData = Contact.findAll()
+   .then((dbUserData) => {
+     if (!dbUserData) {
+       res.status(404).json({ message: "No contacts found" });
+       return;
+     }
+        res.status(200).json(dbUserData);
+   })
+   .catch((err) => {
+        res.status(500).json(err);
+   });
+});
 
 //TODO: Create GET contact by id endpoint
 
