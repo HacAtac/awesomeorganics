1c4edd067f1d1a2345afa25b4530a6a79e898376
HacAtac <jhackworth00@outlook.com>
2023-04-19 10:14:39 -0500

    added login/logout endpoints

--- controllers/api/usersroutes.js
+++ controllers/api/usersroutes.js
@@ -34,4 +34,52 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// POST /api/auth/login
+router.post("/login", async (req, res) => {
+  try {
+    // Find the user by username
+    const user = await User.findOne({ where: { username: req.body.username } });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    // Check the password
+    const validPassword = user.checkPassword(req.body.password);
+    if (!validPassword) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect username or password" });
+    }
+    // Set the user's ID in the session
+    req.session.user_id = user.id;
+    // Send a success response
+    res.status(200).json({ message: `Welcome ${user.username}!` });
+  } catch (err) {
+    console.log("Error logging in a user", err);
+    res.status(500).json(err);
+  }
+});
+
+// POST /api/auth/logout
+router.post("/logout", (req, res) => {
+  if (req.session.user_id) {
+    // Destroy the session
+    req.session.destroy((err) => {
+      if (err) {
+        // Send an error response if there was an issue destroying the session
+        res
+          .status(500)
+          .json({ message: "Failed to log out. Please try again." });
+      } else {
+        // Send a success response with a message
+        res.status(200).json({ message: "Logged out successfully." });
+      }
+    });
+  } else {
+    // Send a response indicating that the user is not logged in
+    res
+      .status(400)
+      .json({ message: "No active session found. Please log in first." });
+  }
+});
+
 module.exports = router;
