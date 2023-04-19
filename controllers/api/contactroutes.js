f55f39dfe3259a55ce9d088c9b5f6d17669f3b06
HacAtac <jhackworth00@outlook.com>
2023-04-18 21:13:49 -0500

    CRUD operations for contact

--- controllers/api/contactroutes.js
+++ controllers/api/contactroutes.js
@@ -31,9 +31,48 @@ const dbUserData = Contact.findAll()
 });
 
 //TODO: Create GET contact by id endpoint
+router.get("/:id", (req, res) => {
+const dbUserData = Contact.findByPk(req.params.id)
+    .then((dbUserData) => {
+     if(!dbUserData){
+     res.status(404).json({ message: `No contact found with id ${req.params.id}`});
+    }
+    res.status(200).json(dbUserData);
+    })
+.catch((err) => {
+    res.status(500).json(err);})
+});
 
 //TODO: Create PUT contact by id endpoint
+router.put("/:id", (req, res) => {
+const dbUserData = Contact.findByPk(req.params.id)
+    .then((dbUserData) => {
+     if(!dbUserData){
+     res.status(404).json({ message: `No contact found with id ${req.params.id}`});
+    }
+    dbUserData.update(req.body);
+    res.status(200).json({ message: `Contact ${req.params.id} updated`, data: dbUserData}
+    );
+    })
+.catch((err) => {
+    res.status(500).json(err);})
+});
 
 //TODO: Create DELETE contact by id endpoint
+router.delete("/:id", (req, res) => {
+  const dbUserData = Contact.findByPk(req.params.id)
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: `No contact found with id ${req.params.id}` });
+        return; // Exit the function to prevent further execution
+      }
+      dbUserData.destroy();
+      res.status(200).json({ message: `Contact ${req.params.id} deleted` }); // Add closing parenthesis here
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    }); // Remove the extra closing brace and parenthesis here
+});
+
 
 module.exports = router;
